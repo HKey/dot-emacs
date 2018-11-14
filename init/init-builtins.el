22fa82d0e56c19c41dd37a7953b45fd1987f40cd
HKey <s06139@gmail.com>
2018-11-15 03:42:39 +0900

    Add init-builtins.el

---
+++ init/init-builtins.el
@@ -0,0 +1,10 @@
+;;; init-builtins.el --- Configuration of builtin features -*- lexical-binding: t; -*-
+
+;;; winner-mode
+
+(winner-mode 1)
+
+
+
+(provide 'init-builtins)
+;;; init-builtins.el ends here
