36f305abed9c60b039b1ed425626268b3e35833a
HKey <s06139@gmail.com>
2019-11-10 00:58:32 +0900

    Catch compiler warning

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((emacs-lisp-mode . ((byte-compile-error-on-warn . t))))
+
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
