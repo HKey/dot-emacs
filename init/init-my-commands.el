5c94213f9eb1a1f836b76cb859146bdee7ba077a
HKey <s06139@gmail.com>
2019-12-19 23:30:01 +0900

    Add init-my-command.el to store my utility commands

---
+++ init/init-my-commands.el
@@ -0,0 +1,21 @@
+;;; init-my-commands.el --- My utility commands   -*- lexical-binding: t; -*-
+
+(require 'init-configuration) ; for loading order
+
+(require 's)
+(require 'f)
+(require 'dash)
+
+
+(defun my-open-gentoo-ebuild (pkg)
+  "Open ebuild file with equery."
+  (interactive "sPackage: ")
+  (--> (shell-command-to-string (concat "equery which " pkg))
+       (s-trim it)
+       (if (f-exists-p it)
+           (find-file it)
+         (user-error "Package not found, %s" pkg))))
+
+
+(provide 'init-my-commands)
+;;; init-my-commands.el ends here
--- script/byte-compile-init.el
+++ script/byte-compile-init.el
@@ -2,4 +2,11 @@
 
 (package-initialize)
 
-(setq byte-compile-error-on-warn t)
+(let* ((this-file (or (buffer-file-name) load-file-name))
+       (prj-root (expand-file-name
+                  (concat (file-name-directory this-file)
+                          "../")))
+       (init (expand-file-name (concat prj-root "init")))
+       (config (expand-file-name (concat prj-root "config"))))
+  (setq byte-compile-error-on-warn t
+        load-path `(,@load-path ,init ,config)))
