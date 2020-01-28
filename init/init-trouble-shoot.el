1055e60b8295b9fc72863096c874e6701a31c992
HKey <s06139@gmail.com>
2020-01-28 13:50:02 +0900

    Record all commands for trouble shooting of mozc-temp-convert-dwim

--- init/init-trouble-shoot.el
+++ init/init-trouble-shoot.el
@@ -7,25 +7,32 @@
 ;; Sometimes the cursor jumps for the bottom of the current window.
 ;; It may be my mistake pressing M-r when I would call `mozc-temp-convert-dwim'
 ;; bound to M-n.
-;; So I add a logging advice to `move-to-window-line-top-bottom' and `mozc-temp-convert-dwim'
-;; to log that whether I pressed M-r.
+;; So I add a logging function which records command name, key sequence
+;; and time.
 
-;; Sometimes the cursor jumps for the bottom of the current window.
-;; It may be caused by my mistake that is pressing M-r when I would call
-;; `mozc-temp-convert-dwim' bound to M-n.
-;; So I add a logging advice to `move-to-window-line-top-bottom' and
-;; `mozc-temp-convert-dwim' to log that whether I pressed M-r.
+(require 'dash)
 
-(defun my-trouble-shoot-log-command-calling (&rest _)
-  (message (concat "Comannd called:\n"
-                   "  command: %s\n"
-                   "  keys:    %s")
-           this-command
-           (format-kbd-macro (this-command-keys))))
+(defvar my-trouble-shoot-log-command-record nil)
 
-(with-eval-after-load 'mozc-temp
-  (advice-add #'move-to-window-line-top-bottom :before #'my-trouble-shoot-log-command-calling)
-  (advice-add #'mozc-temp-convert-dwim :before #'my-trouble-shoot-log-command-calling))
+(defun my-trouble-shoot-log-command-calling ()
+  (push `(:command ,this-command
+                   :key ,(format-kbd-macro (this-command-keys))
+                   :time ,(current-time))
+        my-trouble-shoot-log-command-record))
+
+(defun my-trouble-shoot-show-command-log ()
+  (interactive)
+  (with-current-buffer (get-buffer-create " *my-trouble-shoot-command-log*")
+    (erase-buffer)
+    (--each (reverse my-trouble-shoot-log-command-record)
+      (-let (((&plist :command command :key key :time time) it))
+        (insert "\n"
+                "time: " (format-time-string "%Y-%m-%d %H:%M:%S" time) "\n"
+                " cmd: " (symbol-name command) "\n"
+                " key: " key "\n")))
+    (switch-to-buffer (current-buffer))))
+
+(add-hook 'post-command-hook #'my-trouble-shoot-log-command-calling)
 
 
 (provide 'init-trouble-shoot)
