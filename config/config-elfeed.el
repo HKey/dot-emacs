d8b0185d9e848c66450879d07169ccd7cd7a63f2
HKey <s06139@gmail.com>
2020-04-18 13:54:27 +0900

    [elfeed] Fix org-capture command for elfeed-show

--- config/config-elfeed.el
+++ config/config-elfeed.el
@@ -9,8 +9,8 @@
 (require 'elfeed-show)
 
 ;;; org-capture
-(defun my-org-capture-elfeed ()
-  "Capture current entry of elfeed"
+(defun my-org-capture-elfeed-search ()
+  "Capture current entry of elfeed-search"
   (interactive)
   (let* ((entry (elfeed-search-selected :single))
          (title (elfeed-entry-title entry))
@@ -20,8 +20,19 @@
      (format "%s\n%s" title url))
     (org-capture nil "c")))
 
-(define-key elfeed-search-mode-map (kbd "C") #'my-org-capture-elfeed)
-(define-key elfeed-show-mode-map (kbd "C") #'my-org-capture-elfeed)
+(defun my-org-capture-elfeed-show ()
+  "Capture current entry of elfeed-show"
+  (interactive)
+  (let* ((entry elfeed-show-entry)
+         (title (elfeed-entry-title entry))
+         (url (elfeed-entry-link entry)))
+    ;; pass title and url to org-capture via kill ring
+    (kill-new
+     (format "%s\n%s" title url))
+    (org-capture nil "c")))
+
+(define-key elfeed-search-mode-map (kbd "C") #'my-org-capture-elfeed-search)
+(define-key elfeed-show-mode-map (kbd "C") #'my-org-capture-elfeed-show)
 
 
 (provide 'config-elfeed)
