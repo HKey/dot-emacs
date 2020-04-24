2ac206fff20909057de1f52598ed4fbad0e3d3c2
HKey <s06139@gmail.com>
2020-04-25 01:22:17 +0900

    [prescient] Enable prescient-persist-mode

---
+++ config/config-prescient.el
@@ -0,0 +1,12 @@
+;;; config-prescient.el --- prescient                -*- lexical-binding: t; -*-
+
+(require 'my-bootstrap)
+(use-package prescient)
+
+(require 'prescient)
+
+(prescient-persist-mode 1)
+
+
+(provide 'config-prescient)
+;;; config-prescient.el ends here
