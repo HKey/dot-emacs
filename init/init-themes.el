dae3fa038d0fadb16f212a632a80deda4f9f3339
HKey <s06139@gmail.com>
2020-05-16 22:24:22 +0900

    Add solarized-theme

--- init/init-themes.el
+++ init/init-themes.el
@@ -16,6 +16,20 @@
         spacemacs-theme-org-height nil
         spacemacs-theme-org-priority-bold nil))
 
+;;;; solarized
+
+(use-package solarized
+  :ensure solarized-theme
+  :config
+  (setq solarized-use-variable-pitch  nil
+        solarized-use-less-bold       t
+        solarized-height-minus-1      1.0
+        solarized-height-plus-1       1.0
+        solarized-height-plus-2       1.0
+        solarized-height-plus-3       1.0
+        solarized-height-plus-4       1.0
+        solarized-scale-org-headlines nil))
+
 ;;;; enable theme
 
 (load-theme 'spacemacs-light)
