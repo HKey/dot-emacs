e3987b85727ddef3103b0da99c8ad04450d65e59
HKey <s06139@gmail.com>
2020-05-18 23:38:55 +0900

    [elfeed] Use elfeed tags as org tags for org-capture

--- config/config-elfeed.el
+++ config/config-elfeed.el
@@ -5,34 +5,42 @@
 (use-package elfeed)
 
 (require 'elfeed)
+
+;;;; org-capture
+(use-package dash)
+(use-package s)
+
 (require 'elfeed-search)
 (require 'elfeed-show)
 (require 'elfeed-db)
+(require 'dash)
+(require 's)
 
-;;;; org-capture
+(defun my-org-capture-elfeed--stringify-entry (entry)
+  (let ((title (elfeed-entry-title entry))
+        (url (elfeed-entry-link entry))
+        (tags (-some--> (elfeed-entry-tags entry)
+                (-remove-item 'unread it)
+                (-map #'symbol-name it)
+                (s-join ":" it)
+                (s-wrap it ":" ":"))))
+    (format "%s  %s\n%s" title (or tags "") url)))
+
 (defun my-org-capture-elfeed-search ()
   "Capture current entry of elfeed-search"
   (interactive)
-  (let* ((entry (elfeed-search-selected :single))
-         (title (elfeed-entry-title entry))
-         (url (elfeed-entry-link entry))
-         (feed-title (elfeed-feed-title (elfeed-entry-feed entry))))
-    ;; pass title and url to org-capture via kill ring
-    (kill-new
-     (format "%s\n%s\n\n- %s" title url feed-title))
-    (org-capture nil "c")))
+  ;; pass title and url to org-capture via kill ring
+  (kill-new
+   (my-org-capture-elfeed--stringify-entry (elfeed-search-selected :single)))
+  (org-capture nil "c"))
 
 (defun my-org-capture-elfeed-show ()
   "Capture current entry of elfeed-show"
   (interactive)
-  (let* ((entry elfeed-show-entry)
-         (title (elfeed-entry-title entry))
-         (url (elfeed-entry-link entry))
-         (feed-title (elfeed-feed-title (elfeed-entry-feed entry))))
-    ;; pass title and url to org-capture via kill ring
-    (kill-new
-     (format "%s\n%s\n\n- %s" title url feed-title))
-    (org-capture nil "c")))
+  ;; pass title and url to org-capture via kill ring
+  (kill-new
+   (my-org-capture-elfeed--stringify-entry elfeed-show-entry))
+  (org-capture nil "c"))
 
 (define-key elfeed-search-mode-map (kbd "C") #'my-org-capture-elfeed-search)
 (define-key elfeed-show-mode-map (kbd "C") #'my-org-capture-elfeed-show)
