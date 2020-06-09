0d35ea56c1f49b4f92d0f8f9257c9d00f690e57f
HKey <s06139@gmail.com>
2020-06-10 02:44:19 +0900

    [eww] Add copy commands

--- config/config-eww.el
+++ config/config-eww.el
@@ -8,23 +8,43 @@
 
 (require 'lib-util)
 
+(defun my-eww-copy-title ()
+  (interactive)
+  (let ((title (plist-get eww-data :title)))
+    (kill-new title)))
+
+(defun my-eww-copy-title-and-url ()
+  (interactive)
+  (let ((title (plist-get eww-data :title))
+        (url (plist-get eww-data :url)))
+    (kill-new
+     (format "%s\n%s" title url))))
+
+(defun my-eww-copy-as-org-list ()
+  (interactive)
+  (let ((title (plist-get eww-data :title))
+        (url (plist-get eww-data :url)))
+    (kill-new
+     (format "- %s\n  %s" title url))))
+
 (my-define-key eww-mode-map
   "j" #'scroll-up-line
   "k" #'scroll-down-line
   "H" #'eww-back-url
-  "L" #'eww-forward-url)
+  "L" #'eww-forward-url
+  "yy" #'my-eww-copy-title-and-url
+  "yu" #'eww-copy-page-url
+  "yt" #'my-eww-copy-title
+  "yo" #'my-eww-copy-as-org-list)
 
 ;;;; org-capture
 
 (defun my-org-capture-eww ()
   "Capture current page of eww"
   (interactive)
-  (let ((title (plist-get eww-data :title))
-        (url (plist-get eww-data :url)))
-    ;; pass title and url to org-capture via kill ring
-    (kill-new
-     (format "%s\n%s" title url))
-    (org-capture nil "c")))
+  ;; pass title and url to org-capture via kill ring
+  (my-eww-copy-title-and-url)
+  (org-capture nil "c"))
 
 (define-key eww-mode-map (kbd "C") #'my-org-capture-eww)
 
