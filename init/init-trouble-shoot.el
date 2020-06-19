2be9dbaa27512218578629d009f59c9fe237a9c2
HKey <s06139@gmail.com>
2020-06-19 23:50:38 +0900

    Remove trouble shoot code because its probrem solved by updating ivy.

--- init/init-trouble-shoot.el
+++ init/init-trouble-shoot.el
@@ -40,31 +40,6 @@
 ;; (add-hook 'post-command-hook #'my-trouble-shoot-log-command-calling)
 (add-hook 'pre-command-hook #'my-trouble-shoot-log-command-calling)
 
-;;;; suddenly default-directory changed
-
-;; Sometimes buffer's `default-directory' is changed to not appropriate one.
-(defvar my-trouble-shoot-default-directory (make-hash-table))
-
-(defun my-trouble-shoot-default-directory-post-hook (&rest _)
-  (cl-loop for buffer in (buffer-list)
-           unless (gethash buffer my-trouble-shoot-default-directory)
-           do (puthash buffer
-                       (buffer-local-value 'default-directory buffer)
-                       my-trouble-shoot-default-directory)
-           unless (minibufferp buffer)
-           unless (equal (buffer-local-value 'default-directory buffer)
-                         (gethash buffer my-trouble-shoot-default-directory))
-           do (warn (concat "`default-directory' has been changed\n"
-                            "   command: %s\n"
-                            "    buffer: %s\n"
-                            "  recorded: %s\n"
-                            "   current: %s")
-                    this-command
-                    buffer
-                    (gethash buffer my-trouble-shoot-default-directory)
-                    (buffer-local-value 'default-directory buffer))))
-
-(add-hook 'post-command-hook #'my-trouble-shoot-default-directory-post-hook)
 
 (provide 'init-trouble-shoot)
 ;;; init-trouble-shoot.el ends here
