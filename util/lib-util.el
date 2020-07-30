2556635abedba5942d0b1832ccb223adb7f3a570
HKey <s06139@gmail.com>
2020-07-30 09:05:02 +0900

    Use "declare" to set indentation rule instead of "put"

--- util/lib-util.el
+++ util/lib-util.el
@@ -10,13 +10,13 @@
   "In KEYMAP, assign definitions to keys.
 KEY-DEFINITIONS is a list like below:
   (KEY1 DEF1 KEY2 DEF2 ...)"
+  (declare (indent 1))
   (cl-loop for (key def) on key-definitions by #'cddr
            do (let ((key (if (and (stringp key)
                                   (not (string= key " ")))
                              (read-kbd-macro key)
                            key)))
                 (define-key keymap key def))))
-(put 'my-define-key 'lisp-indent-function 1)
 
 
 (provide 'lib-util)
