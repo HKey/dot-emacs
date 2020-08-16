5e5b299e9ab42175de125256cf800eb521988118
HKey <s06139@gmail.com>
2020-08-16 19:49:22 +0900

    Separate eval-and-compile form in my-bootstrap.el

--- bootstrap/my-bootstrap.el
+++ bootstrap/my-bootstrap.el
@@ -13,10 +13,10 @@
         '((org-taskforecast . "manual")
           (pocket-reader . "manual")))
 
-  (package-initialize)
+  (package-initialize))
 
-
-  ;; package
+;; package
+(eval-and-compile
   (defvar my--with-package-refreshed-p nil)
   (defun my--with-package-ensure-install (package)
     (unless (package-installed-p package)
@@ -24,55 +24,56 @@
         (package-refresh-contents)
         (setq my--with-package-refreshed-p t))
       (package-install package))
-    (add-to-list 'package-selected-packages package))
+    (add-to-list 'package-selected-packages package)))
 
-  (my--with-package-ensure-install 'dash)
+(eval-and-compile
+  (my--with-package-ensure-install 'dash))
 
-  (require 'dash)
+(require 'dash)
 
-  (defmacro my-with-package (package &rest args)
-    (declare (indent 1))
-    (-let* ((err-sym (cl-gensym "err-"))
-            ((&alist :no-install no-install
-                     :when when
-                     :after after
-                     :config: config)
-             (--> (-partition-by-header #'keywordp args)
-                  (--map (if (string-match-p ":.*:$" (symbol-name (car it)))
-                             it
-                           (cons (cl-first it) (cl-second it)))
-                         it)))
-            (config-form
-             (when config
-               `(with-eval-after-load ',package
-                  (condition-case-unless-debug ,err-sym
-                      ,@config
-                    (error
-                     (warn (format "Error at `%s' config, %s"
-                                   ',package
-                                   ,err-sym)))))))
-            (install-form
-             (unless no-install
-               `(eval-and-compile
-                  (my--with-package-ensure-install ',package))))
-            (after-form
-             (--when-let config-form
-               (if after
-                   `(with-eval-after-load ',after ,@it)
-                 `(progn ,@it))))
-            (when-form
-             (when after-form
-               `(when ,when
-                  ,after-form))))
-      `(condition-case-unless-debug ,err-sym
-           ,@(-non-nil (list install-form when-form))
-         (error
-          (warn (format "Error at `%s' my-with-package form, %s"
-                        ',package
-                        ,err-sym))))))
+(defmacro my-with-package (package &rest args)
+  (declare (indent 1))
+  (-let* ((err-sym (cl-gensym "err-"))
+          ((&alist :no-install no-install
+                   :when when
+                   :after after
+                   :config: config)
+           (--> (-partition-by-header #'keywordp args)
+                (--map (if (string-match-p ":.*:$" (symbol-name (car it)))
+                           it
+                         (cons (cl-first it) (cl-second it)))
+                       it)))
+          (config-form
+           (when config
+             `(with-eval-after-load ',package
+                (condition-case-unless-debug ,err-sym
+                    ,@config
+                  (error
+                   (warn (format "Error at `%s' config, %s"
+                                 ',package
+                                 ,err-sym)))))))
+          (install-form
+           (unless no-install
+             `(eval-and-compile
+                (my--with-package-ensure-install ',package))))
+          (after-form
+           (--when-let config-form
+             (if after
+                 `(with-eval-after-load ',after ,@it)
+               `(progn ,@it))))
+          (when-form
+           (when after-form
+             `(when ,when
+                ,after-form))))
+    `(condition-case-unless-debug ,err-sym
+         ,@(-non-nil (list install-form when-form))
+       (error
+        (warn (format "Error at `%s' my-with-package form, %s"
+                      ',package
+                      ,err-sym))))))
 
-  ;; use-package
-
+;; use-package
+(eval-and-compile
   (unless (package-installed-p 'use-package)
     (when (yes-or-no-p "`use-package' package is missing, install it?: ")
       (package-refresh-contents)
