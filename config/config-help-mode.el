0b316be7bea70c58584f776e05d78f60ffaa0ffe
HKey <s06139@gmail.com>
2020-08-16 19:55:24 +0900

    Add config-help-mode.el

---
+++ config/config-help-mode.el
@@ -0,0 +1,20 @@
+;;; config-help.el --- help-mode                     -*- lexical-binding: t; -*-
+
+(require 'my-bootstrap)
+(require 'help-mode)
+
+;; Always select help window when opening it.
+(setq help-window-select t)
+
+;; evil
+(my-with-package evil)
+(require 'evil)
+
+(evil-define-key 'motion help-mode-map
+  ;; Use some keys for default commands not evil commands.
+  (kbd "TAB") (lookup-key help-mode-map (kbd "TAB"))
+  (kbd "q")   (lookup-key help-mode-map (kbd "q")))
+
+
+(provide 'config-help-mode)
+;;; config-help-mode.el ends here
