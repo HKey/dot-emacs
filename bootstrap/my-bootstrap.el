f4f169d148734766d1d00cb1af1a6ddd3d735efa
HKey <s06139@gmail.com>
2020-08-22 17:16:16 +0900

    Remove use-package

--- bootstrap/my-bootstrap.el
+++ bootstrap/my-bootstrap.el
@@ -1,19 +1,17 @@
 ;;; my-bootstrap.el --- bootstrap script             -*- lexical-binding: t; -*-
 
-;; use `eval-and-compile' to install use-package when compiling
-(eval-and-compile
-  (require 'package)
+(require 'package)
 
-  ;; add repositories
-  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-  (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+;; add repositories
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
 
-  ;; pinning
-  (setq package-pinned-packages
-        '((org-taskforecast . "manual")
-          (pocket-reader . "manual")))
+;; pinning
+(setq package-pinned-packages
+      '((org-taskforecast . "manual")
+        (pocket-reader . "manual")))
 
-  (package-initialize))
+(package-initialize)
 
 ;; package
 (defvar my--with-package-refreshed-p nil)
@@ -29,21 +27,6 @@
   (declare (indent 1))
   `(eval-and-compile
      (my--with-package-ensure-install ',package)))
-
-;; use-package
-(eval-and-compile
-  (unless (package-installed-p 'use-package)
-    (when (yes-or-no-p "`use-package' package is missing, install it?: ")
-      (package-refresh-contents)
-      (package-install 'use-package))))
-
-;;; use-package
-
-(require 'use-package)
-
-(setq use-package-always-defer t
-      use-package-always-ensure t
-      use-package-verbose t)
 
 ;;; `load-prefer-newer'
 
