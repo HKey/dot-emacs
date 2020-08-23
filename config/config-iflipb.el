51076f4bab8007822981007797285d49760b3e03
HKey <s06139@gmail.com>
2020-08-24 01:54:23 +0900

    [iflipb] Change buffer name templates

--- config/config-iflipb.el
+++ config/config-iflipb.el
@@ -9,7 +9,9 @@
  ;; don't skip buffers that start with "*"
  iflipb-ignore-buffers nil
  ;; vertical style
- iflipb-format-buffers-function #'iflipb-format-buffers-vertically)
+ iflipb-format-buffers-function #'iflipb-format-buffers-vertically
+ iflipb-other-buffer-template "  %s"
+ iflipb-current-buffer-template "* %s")
 
 
 (provide 'config-iflipb)
