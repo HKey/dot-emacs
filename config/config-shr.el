7325bd183b7315592637b06b43a1a22c25658564
HKey <s06139@gmail.com>
2020-10-27 17:28:41 +0900

    [shr] Add my-shr-rescale-image function

--- config/config-shr.el
+++ config/config-shr.el
@@ -23,6 +23,60 @@
   "Disable underline for sliced images"
   :group 'shr)
 
+(defun my-shr-rescale-image (data content-type width height
+                                  &optional max-width max-height)
+  "Rescale DATA, if too big, to fit the current buffer.
+WIDTH and HEIGHT are the sizes given in the HTML data, if any.
+
+The size of the displayed image will not exceed
+MAX-WIDTH/MAX-HEIGHT.  If not given, use the current window
+width/height instead."
+  ;; if (not (get-buffer-window (current-buffer) t))
+  ;; (create-image data nil t :ascent 100)
+  (let* ((edges (window-inside-pixel-edges
+                 ;; (get-buffer-window (current-buffer))
+                 (selected-window)))
+         ;; (max-width (truncate (* shr-max-image-proportion
+         ;;                         (or max-width
+         ;;                             (- (nth 2 edges) (nth 0 edges))))))
+         ;; (max-height (truncate (* shr-max-image-proportion
+         ;;                          (or max-height
+         ;;                              (- (nth 3 edges) (nth 1 edges))))))
+         (max-width (truncate
+                     (or max-width
+                         (min (* shr-width (default-font-width))
+                              (- (nth 2 edges) (nth 0 edges))))))
+         (max-height (truncate
+                      (or max-height
+                          (min (* 10 (default-font-height))
+                               (- (nth 3 edges) (nth 1 edges))))))
+         (scaling (image-compute-scaling-factor image-scaling-factor)))
+    (when (or (and width
+                   (> width max-width))
+              (and height
+                   (> height max-height)))
+      (setq width nil
+            height nil))
+    (if (and
+         ;; or
+         width height
+         (< (* width scaling) max-width)
+         (< (* height scaling) max-height))
+        (create-image
+         data (shr--image-type) t
+         :ascent 100
+         :width width
+         :height height
+         :format content-type)
+      (create-image
+       data (shr--image-type) t
+       :ascent 100
+       :width width
+       :height height
+       :max-width max-width
+       :max-height max-height
+       :format content-type))))
+
 (defun my-shr-put-image (spec alt &optional flags)
   "Insert image SPEC with a string ALT.  Return image.
 SPEC is either an image data blob, or a list where the first
@@ -45,15 +99,17 @@ element is the data blob and the second element is the content-type."
                         (create-image data 'svg t :ascent 100)))
                      ((eq size 'full)
                       (ignore-errors
-                        (shr-rescale-image data content-type
-                                           (plist-get flags :width)
-                                           (plist-get flags :height))))
+                        (my-shr-rescale-image data content-type
+                                              (plist-get flags :width)
+                                              (plist-get flags :height))))
                      (t
                       (ignore-errors
-                        (shr-rescale-image data content-type
-                                           (plist-get flags :width)
-                                           (plist-get flags :height)))))))
-        (when image
+                        (my-shr-rescale-image data content-type
+                                              (plist-get flags :width)
+                                              (plist-get flags :height)))))))
+        (when (and image
+                   ;; gif makes emacs slow
+                   (not (image-multi-frame-p image)))
           ;; When inserting big-ish pictures, put them at the
           ;; beginning of the line.
           (when (and (> (current-column) 0)
@@ -76,6 +132,7 @@ element is the data blob and the second element is the content-type."
     (insert (or alt ""))))
 
 (setq shr-put-image-function #'my-shr-put-image)
+;; (setq shr-put-image-function #'shr-put-image)
 
 
 (provide 'config-shr)
