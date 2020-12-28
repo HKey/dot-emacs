006fa35a2a01ff8f231d1734e294f13f932c6857
HKey <s06139@gmail.com>
2020-12-28 23:19:14 +0900

    Recenter also when window configuration is changed

--- init/my-init-configuration.el
+++ init/my-init-configuration.el
@@ -506,9 +506,13 @@
 (define-minor-mode my-always-recenter-mode
   "Always recentering mode."
   :init-value nil
-  (if my-always-recenter-mode
-      (add-hook 'post-command-hook #'my-always-recenter--recenter nil t)
-    (remove-hook 'post-command-hook #'my-always-recenter--recenter t)))
+  (let ((hooks '(post-command-hook
+                 window-configuration-change-hook)))
+    (if my-always-recenter-mode
+        (--each hooks
+          (add-hook it #'my-always-recenter--recenter 100 t))
+      (--each hooks
+        (remove-hook it #'my-always-recenter--recenter t)))))
 
 (define-globalized-minor-mode my-global-always-recenter-mode
   my-always-recenter-mode (lambda () (my-always-recenter-mode 1))
