911d0f15d1e78dd876c0e5352cfb8fa83a519511
HKey <s06139@gmail.com>
2021-01-06 16:16:22 +0900

    [org-ql] Define my utility commands

--- config/my-config-org-ql.el
+++ config/my-config-org-ql.el
@@ -12,6 +12,32 @@
 (setq org-ql-view-display-buffer-action
       (cons #'display-buffer-same-window nil))
 
+;;; my commands
+
+(my-with-package transient)
+(require 'transient)
+
+(defun my-org-ql-next-actions ()
+  "Search next actions in this buffer."
+  (interactive)
+  (org-ql-search (current-buffer)
+    '(and (todo)
+          (not (org-entry-blocked-p))
+          (not (or (scheduled) (deadline))))))
+
+(defun my-org-ql-recent-items ()
+  "Search recent items in this buffer."
+  (interactive)
+  (org-ql-view-recent-items :num-days 3 :files (current-buffer)))
+
+(transient-define-prefix my-org-ql-transient ()
+  [["Query for this buffer"
+    ("r" "recent items" my-org-ql-recent-items)
+    ("n" "next actions" my-org-ql-next-actions)]]
+  ["Transient"
+   [("q" "quit" transient-quit-one)]
+   [("<escape>" "quit" transient-quit-one)]])
+
 
 (provide 'my-config-org-ql)
 ;;; my-config-org-ql.el ends here
