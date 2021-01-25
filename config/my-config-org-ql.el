c4d13792eb8eac31c21d5482eadcbff179bc3173
HKey <s06139@gmail.com>
2021-01-26 07:33:23 +0900

    Add some commands to my-org-ql-transient

--- config/my-config-org-ql.el
+++ config/my-config-org-ql.el
@@ -17,13 +17,31 @@
 (my-with-package transient)
 (require 'transient)
 
+(defconst my-org-ql--query-next-actions
+  '(and (todo)
+        (not (org-entry-blocked-p))
+        (not (or (scheduled) (deadline)))))
+
 (defun my-org-ql-next-actions ()
   "Search next actions in this buffer."
   (interactive)
   (org-ql-search (current-buffer)
+    my-org-ql--query-next-actions))
+
+(defun my-org-ql-next-actions-sparse-tree ()
+  "Show sparse tree of next actions in this buffer."
+  (interactive)
+  (org-ql-sparse-tree
+   my-org-ql--query-next-actions))
+
+(defun my-org-ql-completed-project ()
+  "Search completed but todo projects in this buffer."
+  (interactive)
+  (org-ql-search
+    (org-agenda-files)
     '(and (todo)
-          (not (org-entry-blocked-p))
-          (not (or (scheduled) (deadline))))))
+          (not (descendants (todo)))
+          (descendants (done)))))
 
 (defun my-org-ql-recent-items ()
   "Search recent items in this buffer."
@@ -33,7 +51,11 @@
 (transient-define-prefix my-org-ql-transient ()
   [["Query for this buffer"
     ("r" "recent items" my-org-ql-recent-items)
-    ("n" "next actions" my-org-ql-next-actions)]]
+    ("n" "next actions" my-org-ql-next-actions)
+    ("P" "completed projects" my-org-ql-completed-project)]
+   ["Sparse tree for this buffer"
+    ("sn" "next actions sparse tree" my-org-ql-next-actions-sparse-tree)
+    ("sq" "show all (exit sparse tree)" org-show-all)]]
   ["Transient"
    [("q" "quit" transient-quit-one)]
    [("<escape>" "quit" transient-quit-one)]])
