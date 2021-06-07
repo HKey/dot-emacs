4d9427138781204ac4ac35a3a48a6dc83e45785b
HKey <s06139@gmail.com>
2021-06-07 22:57:26 +0900

    Remove racer

--- config/my-config-rust-mode.el
+++ config/my-config-rust-mode.el
@@ -2,18 +2,15 @@
 
 (require 'my-bootstrap)
 (my-with-package rust-mode)
-(my-with-package racer)
 (my-with-package flycheck)
 (my-with-package flycheck-rust)
 
 (require 'rust-mode)
-(require 'racer)
 (require 'flycheck)
 (require 'flycheck-rust)
 
 (defun my-rust-mode-hook ()
   (eldoc-mode 1)
-  (racer-mode 1)
   (flycheck-rust-setup)
   (flycheck-mode 1))
 
